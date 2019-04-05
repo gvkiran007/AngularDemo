20e069690de86c1f498bc30ae1b6dcdc27bd2d1a
gvkiran007 <gvkiran007@gamil.com>
2019-04-05 13:13:45 +0530

    aaded new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,   
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
